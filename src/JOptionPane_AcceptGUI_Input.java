5f63ea70397ecffa682fd4476d946aed9ee6cb9b
Leian <Leian@DESKTOP-9DBMGK5>
2020-05-16 13:39:11 -0700

    [2020.05.16] until Book Page#87

--- src/JOptionPane_AcceptGUI_Input.java
+++ src/JOptionPane_AcceptGUI_Input.java
@@ -51,25 +51,6 @@ public class JOptionPane_AcceptGUI_Input {
 		
 		
 		
-		// Page 86 : Using Confirm Dialog Boxes
-		
-		
-		int selection;
-		boolean isYes;
-		
-		selection = JOptionPane.showConfirmDialog(null,  "Do you want to upgrade to first class?");
-		isYes = (selection == JOptionPane.YES_OPTION);	// if user selected 'yes', selection will be same with yes, and it will be also 'True'.
-		JOptionPane.showMessageDialog(null,  "You responded " + isYes);
-		
-		
-		  // confirm dialog box's five arguments
-		  // = parent component, prompt message, title, integer that indicates which option button will be shown, the kind of dialog box 
-		  // ex..
-		
-		JOptionPane.showConfirmDialog(null,  "A data input error has occurred. Continue?", "Data input error", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
-		
-		
-		// 88페이지부터 볼것.
 		
 		
 		

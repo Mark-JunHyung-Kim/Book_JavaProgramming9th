8f54631e94461ed80fd4dc0b8c952bf689b24ce6
Leian <Leian@DESKTOP-9DBMGK5>
2020-05-15 20:28:19 -0700

    [2020.05.15] S : Dialog Box in Java

---
+++ src/GUI_dialogBox.java
@@ -0,0 +1,14 @@
+// Date : 2020.05.15
+// Name : Mark JunHyung Kim
+// Subject : How to make dialogBox (in GUI Output)
+
+import javax.swing.JOptionPane;
+public class GUI_dialogBox {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "This is Java dialog");
+
+	}
+
+}

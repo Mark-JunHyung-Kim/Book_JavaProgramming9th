1abb0383e2a8f4b6cbf993ce58aeb13830cbeb65
Mark-JunHyung-Kim <markgraykim@gmail.com>
2020-06-17 22:42:22 -0700

    [2020.06.17] Until Lecture-07 Page#04

---
+++ src/CSIS2175/L_Chapter_9/P004_SwapTwoNumbers.java
@@ -0,0 +1,30 @@
+package CSIS2175.L_Chapter_9;
+//Date : 2020.06.17
+//Name : Mark JunHyung Kim
+//Chapter : Lecture#07 = Chapter#9
+//Subject : P004 - Swap two numbers
+
+public class P004_SwapTwoNumbers {
+
+	public static void main(String[] args) 
+	{
+		int temp, valA, valB;
+		
+		valA = 16;
+		valB = 2;
+		
+		System.out.println("valA is : " + valA + " and valB is : " + valB);
+		
+		temp = valA;
+		valA = valB;
+		valB = temp;
+		
+		System.out.println("valA is : " + valA + " and valB is : " + valB);
+		
+		// 나머지는 내일부터 이어서
+		
+
+	}
+	
+
+}

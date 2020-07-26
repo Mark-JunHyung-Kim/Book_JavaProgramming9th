96d06a44518062eb50d04a9a5b18279e7a2d11dd
Mark-JunHyung-Kim <markgraykim@gmail.com>
2020-07-25 22:49:52 -0700

    [2020.07.25] Until Lecture-12 Page#012

---
+++ src/CSIS2175/L_Chapter_12/P012_JFrame1.java
@@ -0,0 +1,19 @@
+package CSIS2175.L_Chapter_12;
+//Date : 2020.07.25
+//Name : Mark JunHyung Kim
+//Chapter : Lecture#12 = Chapter#12
+//Subject : P012 - JFrame1
+
+import javax.swing.JFrame;
+public class P012_JFrame1 
+{
+
+	public static void main(String[] args) 
+	{
+		JFrame aFrame = new JFrame("First frame");
+		aFrame.setSize(250, 100);
+		aFrame.setVisible(true);
+
+	}
+
+}
